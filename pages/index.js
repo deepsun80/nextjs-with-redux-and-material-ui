a8d566067507c5f9972475e13e5be75e61cf2d50
Sandeep Chandran <deepsun80@gmail.com>
2019-04-28 08:27:43 -0500

    test

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
@@ -20,7 +20,7 @@ const styles = theme => ({
   }
 })
 
-class Index extends PureComponent {
+class Index extends Component {
   static getInitialProps ({ store, isServer }) {
     store.dispatch(increment(isServer))
 
